61b3e3496246b2497e695e087c358549deac60a9
agus gunawan <62685061+Agussgunawan@users.noreply.github.com>
2020-04-25 14:11:49 +0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -1,4 +1,11 @@
+Import java.util.Scanner;
+
 public class InputOutput{
   public static void main(String [] args){
+   Scanner scanner = new Scanner(System.in)
+   String nama;
+   System.out.print("Muhammad Agus Gunawan: ");
+   nama = scanner.nextLine();
+   System.out.println("Hai, "+Muhammad Agus Gunawan);
   }
 }  
